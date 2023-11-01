06b00c16448cdae90949e058fa330fa345af459f
Henry Laurent Khosasih <henrykhosasih@TrickyPanda.local>
2023-11-01 14:25:00 +1100

    add contact us page

---
+++ app/about/contact-us/page.tsx
@@ -0,0 +1,47 @@
+import ContactUsContent from "@/components/ContactUsContent";
+import ContactUsForm from "@/components/ContactUsForm";
+import Image from "next/image";
+
+function ContactUs() {
+  return (
+    <section id="e-referral" className="flex flex-col">
+      {/* Image, title */}
+      <div className="relative flex flex-col justify-center items-center h-80 lg:h-96">
+        <Image
+          src={"https://picsum.photos/id/502/1600/500"}
+          alt={"request appointment background"}
+          fill
+          className="absolute object-cover opacity-50 -z-10"
+        />
+        <div className="flex flex-col items-center gap-2 bg-white p-3">
+          <h1 className="text-3xl lg:text-5xl text-gray-950">Contact Us</h1>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 p-20 px-5 md:px-20 gap-10 max-w-7xl mx-auto">
+        <div className="flex flex-col space-y-7 lg:justify-center">
+          <div className="space-y-5 text-center lg:text-left">
+            <h2 className="text-2xl lg:text-3xl lg:w-[70%]">
+              Contact our support team to get more info
+            </h2>
+            <p className="text-lg">
+              Need more information or have specific inquiries? Our dedicated
+              support team is here to assist you. Whether you have questions
+              about our services, need guidance on test options, or require
+              clarification on any aspect, we're just a message or phone call
+              away.
+            </p>
+          </div>
+          <div className="border-b border-black"></div>
+          <ContactUsContent />
+        </div>
+        <div className="p-5 rounded-md">
+          <h3 className="text-center text-2xl lg:text-3xl p-5">
+            Send us a message
+          </h3>
+          <ContactUsForm />
+        </div>
+      </div>
+    </section>
+  );
+}
+export default ContactUs;
--- components/ContactUsCard.tsx
+++ components/ContactUsCard.tsx
@@ -1,13 +1,12 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import ContactUsContent from "./ContactUsContent";
 import { Button } from "./ui/button";
-import { Mail, MapPin, Phone } from "lucide-react";
 
 function ContactUsCard() {
   return (
@@ -16,23 +15,7 @@ function ContactUsCard() {
         <CardTitle>Have more questions?</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-3 items-center">
-            <MapPin className="w-10 basis-1/12" />
-            <p className="basis-11/12">
-              Shop RP25E Roxburgh Village, 250 Somerton Road, Roxburgh Park, VIC
-              3064
-            </p>
-          </div>
-          <div className="flex gap-3 items-center">
-            <Mail className="w-10 basis-1/12" />
-            <p className="basis-11/12">info@inoneimaging.com.au</p>
-          </div>
-          <div className="flex gap-3 items-center">
-            <Phone className="w-10 basis-1/12" />
-            <p className="basis-11/12">+61 3 9063 0200</p>
-          </div>
-        </div>
+        <ContactUsContent />
       </CardContent>
       <CardFooter>
         <Button className="w-full">Contact us</Button>
