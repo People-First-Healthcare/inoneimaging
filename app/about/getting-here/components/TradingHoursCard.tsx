e4f7a497321c0f242cab52ba09ca43af6a4a5e90
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2023-11-23 19:54:37 +1100

    style trading hours card

--- app/about/getting-here/components/TradingHoursCard.tsx
+++ app/about/getting-here/components/TradingHoursCard.tsx
@@ -12,20 +12,20 @@ const tradingHours = [
 
 function TradingHoursCard() {
   return (
-    <div className="border border-black p-4 h-fit">
+    <div className="border border-black p-4 h-fit col-span-3 lg:col-span-1">
       <div className="flex flex-row justify-center py-5">
         <TimerIcon />
         <p>Our trading hours</p>
       </div>
       {tradingHours.map((dayOfWeek, i) => (
-        <div key={i} className="grid grid-cols-2 border-b p-2">
+        <div key={i} className="grid grid-cols-2 border-b p-2 text-center">
           <p>{dayOfWeek.day}</p>
           <p>
             {dayOfWeek.open} - {dayOfWeek.close}
           </p>
         </div>
       ))}
-      <div className="grid grid-cols-2 p-2">
+      <div className="grid grid-cols-2 p-2 text-center">
         <p>Holidays</p>
         <p>Close</p>
       </div>
