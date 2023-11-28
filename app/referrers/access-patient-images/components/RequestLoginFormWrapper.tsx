e70085b3909f933515cefaf32fe26a6502cdcab0
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2023-11-28 17:04:19 +1100

    fix escape char

--- app/referrers/access-patient-images/components/RequestLoginFormWrapper.tsx
+++ app/referrers/access-patient-images/components/RequestLoginFormWrapper.tsx
@@ -13,8 +13,8 @@ function RequestLoginFormWrapper() {
         <div className="flex flex-col gap-5 text-center items-center grow">
           <CheckIcon className="bg-green-400 text-white rounded-full p-2 h-10 w-10" />
           <h2 className="text-2xl">
-            Thank you for requesting access to your patient's reports and
-            images.
+            {`Thank you for requesting access to your patient's reports and
+            images.`}
           </h2>
           <p>Our friendly team will be in touch with you shortly.</p>
         </div>
