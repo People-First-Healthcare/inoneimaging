3cbd78e1f35b2b570e9a731375d81710107bf389
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2023-12-14 10:06:28 +1100

    fix sidenav buggy scroll

--- components/SideNavContent.tsx
+++ components/SideNavContent.tsx
@@ -25,7 +25,7 @@ function SideNavContent({ menu, handleLeave }: Props) {
     <motion.div
       key={"sideNav"}
       {...sideNavAnimation}
-      className="flex flex-col space-y-24 px-14 pb-36 h-screen w-full bg-white z-10 overflow-y-scroll"
+      className="flex flex-col space-y-24 px-14 pb-36 h-screen w-full bg-white z-10"
     >
       <div className="space-y-5">
         {menu.map((menuItem, i) => (
