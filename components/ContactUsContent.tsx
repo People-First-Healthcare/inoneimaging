5939c90f8f6ff368d56ca4b3c3953fae88167a8e
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2023-12-16 11:18:14 +1100

    update links

--- components/ContactUsContent.tsx
+++ components/ContactUsContent.tsx
@@ -1,23 +1,34 @@
 import { Mail, MapPin, Phone } from "lucide-react";
+import Link from "next/link";
 
 function ContactUsContent() {
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex gap-3 items-center">
+      <Link
+        href={"https://maps.app.goo.gl/RBscTfw1Kbzy9Gfm8"}
+        target="_blank"
+        className="flex gap-3 items-center"
+      >
         <MapPin className="w-10 basis-1/12" />
+
         <p className="basis-11/12">
           Shop RP25E Roxburgh Village, 250 Somerton Road, Roxburgh Park, VIC
           3064
         </p>
-      </div>
-      <div className="flex gap-3 items-center">
+      </Link>
+
+      <Link
+        href={`mailto:${"info@inoneimaging.com.au"}`}
+        className="flex gap-3 items-center"
+      >
         <Mail className="w-10 basis-1/12" />
         <p className="basis-11/12">info@inoneimaging.com.au</p>
-      </div>
-      <div className="flex gap-3 items-center">
+      </Link>
+
+      <Link href={`tel:0390630200`} className="flex gap-3 items-center">
         <Phone className="w-10 basis-1/12" />
         <p className="basis-11/12">(03) 9063 0200</p>
-      </div>
+      </Link>
     </div>
   );
 }
