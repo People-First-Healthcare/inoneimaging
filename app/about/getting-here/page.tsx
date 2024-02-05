645fe30a4207e6bea53babced2b278ef07e485cb
IT-PFHCare <154285525+IT-PFHCare@users.noreply.github.com>
2024-02-05 13:28:33 +1100

    page.tsx

--- app/about/getting-here/page.tsx
+++ app/about/getting-here/page.tsx
@@ -30,9 +30,7 @@ function GettingHerePage() {
       <div className="relative">
       <map />
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12637.741516028012!2d144.9328315!3d-37.6389671!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65179eb7531d9%3A0xf504418d6c371f23!2sIn%20One%20Healthcare!5e0!3m2!1sen!2sau!4v1707099782783!5m2!1sen!2sau" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-        <Button className="absolute top-[70%] left-1/2 transform -translate-x-1/2 w-72">
-            Get Directions
-          </Button>
+        
      </iframe>
 
         <CopyAddressButton classname="absolute top-[82%] left-1/2 transform -translate-x-1/2 w-72" />
