335da2b5132bbeff3a03e80ccbc226c059488e54
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2024-05-02 15:53:16 +1000

    feat: add request referral pads form

--- app/referrers/request-referral-pads/page.tsx
+++ app/referrers/request-referral-pads/page.tsx
@@ -1,45 +1,19 @@
-import RequestReferralPadsFormWrapper from "./components/RequestReferralPadsFormWrapper";
 import HeroBanner from "@/components/HeroBanner";
 import { Metadata } from "next";
-import ContactUsCard from "@/components/ContactUsCard";
-import Image from "next/image";
+import RequestReferralPadsFormWrapper from "./components/RequestReferralPadsFormWrapper";
 
 export const metadata: Metadata = {
-  title: "Order Referrer Pads",
+  title: "Order Referral Pads",
   description:
     "Referrers can conveniently request referral pads through this form. Follow the steps and request for referrer pads now.",
   alternates: {
-    canonical: "/referrers/request-referrer-pads",
+    canonical: "/referrers/request-referral-pads",
   },
 };
 
 function RequestReferralPads() {
   return (
-    // <section id="e-referral" className="flex flex-col">
-    //   {/* Image, title */}
-    //   <HeroBanner
-    //     title={"Request Referral Pads"}
-    //     image={"/images/contents/reception.png"}
-    //   />
-
-    //   <div className="flex flex-col lg:flex-row items-center px-4 py-10 gap-10 lg:gap-16 mx-auto">
-    //     {/* Form */}
-    //     <div className="flex flex-col gap-5 max-w-xl">
-    //       <h2 className="text-2xl">Request an Appointment</h2>
-    //       <RequestReferralPadsFormWrapper />
-    //     </div>
-
-    //     {/* Photo */}
-    //     <Image
-    //       src={"/images/contents/penpaper.png"}
-    //       width={400}
-    //       height={500}
-    //       alt="A happy patient waiting for an appointment"
-    //       className="rounded-xl"
-    //     />
-    //   </div>
-    // </section>
-    <section id="e-referral" className="flex flex-col">
+    <section id="request-referral-pads" className="flex flex-col">
       {/* Image, title */}
       <HeroBanner
         title={"Request Referral Pads"}
